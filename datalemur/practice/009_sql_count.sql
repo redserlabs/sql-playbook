22e35c350bf0955b2548d61861522ac69bad4613
Reda Serhir <ton_mail_github@exemple.com>
2026-03-06 01:47:42 +0100

    Add DataLemur practice 009 (COUNT)

---
+++ datalemur/practice/009_sql_count.sql
@@ -0,0 +1,12 @@
+-- Title: SQL COUNT Practice Exercise
+-- Source: DataLemur (Practice)
+-- Link: https://datalemur.com/questions/sql-count-practice-exercise
+-- Concepts: COUNT(*)
+
+/*
+Approach:
+- COUNT(*) returns the number of rows in the table
+*/
+
+SELECT COUNT(*)
+FROM pharmacy_sales;
